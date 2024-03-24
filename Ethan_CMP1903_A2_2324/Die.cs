ca057ad654515074d973e3ea8b0a0e844d818d33
SeeTwoH6 <ethan.caballero@yahoo.com>
2024-03-24 19:40:18 +0000

    Added Die class from assess 1

---
+++ Ethan_CMP1903_A2_2324/Die.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ethan_CMP1903_A2_2324
+{
+    internal class Die
+    {
+        /*
+         * The Die class should contain one property to hold the current die value,
+         * and one method that rolls the die, returns and integer and takes no parameters.
+         */
+        //Property
+        public int Roll { get; set; } //Property definition improvement from the review process
+        //Method
+        private static Random roll = new Random();
+
+        /// <summary>
+        /// Rolls a random number between 1 and 6 inclusively
+        /// </summary>
+        /// <returns>Returns a single integer</returns>
+        public int RollDice()
+        {
+            Roll = roll.Next(1, 6);
+            return Roll;
+        }
+    }
+}
