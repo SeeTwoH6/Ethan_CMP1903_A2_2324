3c676d5f498fffae38986cab04272e87eb148aba
SeeTwoH6 <ethan.caballero@yahoo.com>
2024-04-23 00:43:43 +0100

    Added some user input to 3ormore

--- Ethan_CMP1903_A2_2324/ThreeOrMore.cs
+++ Ethan_CMP1903_A2_2324/ThreeOrMore.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Ethan_CMP1903_A2_2324
 {
@@ -22,6 +23,8 @@ namespace Ethan_CMP1903_A2_2324
         }
         public void StartGame()
         {
+            score = 0;
+            turns = 0;
             for (int i = 6; i >= 1 ; i--) //1-6 represents each possibility of dice rolls, starting of 6 because we're hoping to catch the highest duplicates first
             {
                 List<int> listOfIndices = new List<int>();
@@ -39,24 +42,53 @@ namespace Ethan_CMP1903_A2_2324
                     }
                     index++;
                 }
+
+                int choice = 0;
                 if (count >= 2)
                 {
-                    Console.WriteLine($"You rolled at least two {i}'s\nDo you want to:\n1) Reroll the rest of your dice?\n2) Reroll all of your dice\n3)Proceed with point calculation");
+                    while (choice < 1 || choice > 3)
+                    {
+                        Console.WriteLine($"You rolled at least two {i}'s\nDo you want to:\n1) Reroll the rest of your dice?\n2) Reroll all of your dice\n3)Proceed with point calculation");
+                        try
+                        {
+                            choice = Int32.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
                 }
-                //Reroll the rest of the dice
-                index = 0;
-                foreach (Die die in DieList) //Go through each dice in the list again
+
+                if (choice == 1)
                 {
-                    if (index != listOfIndices.First() || index != listOfIndices.Last()) //If the current index is not any of the two items in the list (items in the list are there 2 of a kind)
+                    //Reroll the rest of the dice
+                    index = 0;
+                    foreach (Die die in DieList) //Go through each dice in the list again
+                    {
+                        if (index != listOfIndices.First() || index != listOfIndices.Last()) //If the current index is not any of the two items in the list (items in the list are there 2 of a kind)
+                        {
+                            die.RollDice();
+                        }
+                        index++;
+                    }
+                }
+
+                else if (choice == 2)
+                {
+                    //Reroll all dice
+                    foreach (Die die in DieList)
                     {
                         die.RollDice();
                     }
-                    index++;
                 }
-                //Reroll all dice
-                foreach (Die die in DieList)
+
+                else //Point calculation
                 {
-                    die.RollDice();
+                    foreach (Die die in DieList)
+                    {
+                        score += die.Roll;
+                    }
                 }
             }
         }
